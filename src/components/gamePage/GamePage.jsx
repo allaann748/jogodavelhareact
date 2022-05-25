6f282b5161091c80526d4e697f613cf756119f73
alan <alanunesalan@outlook.com>
2022-05-25 15:11:51 -0300

    jogo

---
+++ src/components/gamePage/GamePage.jsx
@@ -0,0 +1,13 @@
+import Title from '../title/Title'
+import Subtitle from '../subtitle/Subtitle'
+
+function GamePage () {
+    return (
+        <div className="container">
+         <Title>Jogo da velha</Title>
+         <Subtitle>Alan Nunes</Subtitle>
+        </div>
+    )
+}
+
+export default GamePage
