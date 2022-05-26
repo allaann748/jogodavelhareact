e77605e21ced64a7ec43ee79c45efcba481cb17a
alan <alanunesalan@outlook.com>
2022-05-26 14:29:53 -0300

    git

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,16 @@
 import Title from '../title/Title'
 import Subtitle from '../subtitle/Subtitle'
 import styles from './Header.module.css'
+import Icon from '../icon/Icon'
 
 function Header () {
     return (
       <div className={styles.header}>
         <Title>Jogo da velha</Title>
         <Subtitle>Alan Nunes</Subtitle>
+        <div className={styles.iconContent}>
+            <Icon iconName="github" link="https://github.com/allaann748"/>
+        </div>
       </div>
     )
 }
