46d4ec1e63533a3db304a5c58d78fe63d1d5d984
alan <alanunesalan@outlook.com>
2022-08-04 11:14:05 -0300

    atualizar

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -4,13 +4,25 @@ import { useState } from 'react'
 
 function Game () {
     const [gameState, setGameState] = useState(Array(9).fill(0))
-        return (
+    const [currentPlayer, setCurrentPlayer] = useState(1)
+
+    const handleclick = (pos) => {
+      if (gameState[pos] === 0){
+        let newGameState = [...gameState]
+        newGameState[pos] = currentPlayer
+        setCurrentPlayer(currentPlayer * -1)
+        setGameState(newGameState)
+      }
+     }
+
+         return (
         <div className={styles.game}>
             {
                 gameState.map((value, pos) =>
                 <GameOption 
                 key={`game-option-pos-${pos}`}
                 status={value}
+                onClick={() => handleclick(pos)}
                 />
              )
             }
